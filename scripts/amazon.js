1adfd22c206ad5af3d55cddfbdd863d321200cc5
Talha <tssmart233@gmail.com>
2024-08-23 20:41:47 +0500

    Make The Cart Quantity Interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -13,14 +13,14 @@ products.forEach((product) => {
 
           <div class="product-rating-container">
             <img class="product-rating-stars"
-              src="${product.rating.stars * 10}">
+              src="images/ratings/rating-${product.rating.stars * 10}.png">
             <div class="product-rating-count link-primary">
               ${product.rating.count}
             </div>
           </div>
 
-          <div class="product-price"> 
-            $${product.priceCents / 100}
+          <div class="product-price">
+            $${(product.priceCents / 100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
@@ -45,10 +45,42 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart"
+           data-product-id="${product.id}">
             Add to Cart
           </button>
-        </div>`       
+        </div>`
         
-});
+      });
+      
       document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+      document.querySelectorAll('.js-add-to-cart')
+      .forEach((button) => {
+        button.addEventListener('click', () => {
+        const productId = button.dataset.productId;
+
+        let matchingItem;
+        cart.forEach((item) => {
+          if(productId === item.productId) {
+            matchingItem = item;
+          }
+        });
+
+        if(matchingItem){
+          matchingItem.quantity += 1;
+        }
+        else{
+          cart.push({
+          productId: productId,
+          quantity: 1
+          })
+        }
+        let cartQuantity = 0;
+        cart.forEach((item)=>{
+          cartQuantity += item.quantity
+        })
+
+        document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+      });
+    });
