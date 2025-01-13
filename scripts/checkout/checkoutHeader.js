c5eb50dbba581f42aa2e00f8c49116c4337e4617
Muhammad Talha Shaikh <161719931+TalhaShaikh150@users.noreply.github.com>
2025-01-13 16:14:37 +0500

    Update checkoutHeader.js

--- scripts/checkout/checkoutHeader.js
+++ scripts/checkout/checkoutHeader.js
@@ -10,17 +10,17 @@ export function renderCheckoutHeader() {
   const checkoutHeaderHTML = `
     <div class="header-content">
       <div class="checkout-header-left-section">
-        <a href="amazon.html">
-          <img class="amazon-logo" src="images/amazon-logo.png">
-          <img class="amazon-mobile-logo" src="images/amazon-mobile-logo.png">
+        <a href="index.html">
+          <img class="amazon-logo" src="./images/amazon-logo.png">
+          <img class="amazon-mobile-logo" src="./images/amazon-mobile-logo.png">
         </a>
       </div>
       <div class="checkout-header-middle-section">
         Checkout (<a class="return-to-home-link"
-          href="amazon.html">${cartQuantity} items</a>)
+          href="index.html">${cartQuantity} items</a>)
       </div>
       <div class="checkout-header-right-section">
-        <img src="images/icons/checkout-lock-icon.png">
+        <img src="./images/icons/checkout-lock-icon.png">
       </div>
     </div>
   `;
